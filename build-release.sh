9600b4c463babddc3500cda216b6799191d5661b
Dylan Reimerink <dylan@serverius.net>
2020-11-08 20:22:50 +0100

    Fixed release build script

--- build-release.sh
+++ build-release.sh
@@ -11,8 +11,8 @@ mkdir -p release
 rm -f release/*
 
 CGO_ENABLED=0 gox \
-    -osarch="!darwin/386" \ # 32-bit darwin is no longer supported in go1.15 https://golang.org/doc/go1.15#darwin
-    -output="release/{{.Dir}}_$VERSION_{{.OS}}_{{.Arch}}" \
-    -ldflags "-X main.TlsToolVersion=$VERSION" \
-    -ldflags "-X main.TlsToolCommitHash=$GIT_COMMIT" \
-    -ldflags "-X main.TlsToolGolangBuildVersion=$GOLANG_VERSION"
+    -osarch="!darwin/386" \
+    -ldflags "-X main.TlsToolVersion='$VERSION' \
+    -X main.TlsToolCommitHash='$GIT_COMMIT' \
+    -X main.TlsToolGolangBuildVersion='$GOLANG_VERSION'" \
+    -output="release/{{.Dir}}_$VERSION_{{.OS}}_{{.Arch}}" 
